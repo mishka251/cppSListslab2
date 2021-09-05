8d92e7ce1796443f384270ac67dce0f6df626b79
mishka251 <mishkabelka251@gmail.com>
2021-09-05 16:04:22 +0500

    комментарии

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-struct List {
+struct List {  //
     int value;
     List *next;
-    List *prev;
+    List *prev; // указатель на предыдущ элемент
 };
 
 
@@ -22,7 +22,7 @@ void initList() {
         last->value = a;
         if (i < n - 1) {
             List *newItem = new List();
-            newItem->prev = last;
+            newItem->prev = last; // предыдущ элемент для нового это последний
             last->next = newItem;
             last = last->next;
         }
@@ -39,43 +39,43 @@ void printList() {
 }
 
 
-void addToList(List *item, int toBegin, int toLast) {
-    List *newFirst = new List();
-    newFirst->value = toBegin;
+void addToList(List *item, int toBegin, int toLast) { // получает указатель на элемент списка и два значения которые надо вставить в начало и конец
+    List *newFirst = new List(); // создаём новый первый элемент
+    newFirst->value = toBegin; //записываем то что надо  вставить в начало
 
-    List *newLast = new List();
-    newLast->value = toLast;
+    List *newLast = new List(); // создаем новый последний элемент
+    newLast->value = toLast; // записываем
 
-    List *lastItem = item;
-    while (lastItem->next != nullptr) {
-        lastItem = lastItem->next;
+    List *lastItem = item; // создаем указатель на последн элемент списка
+    while (lastItem->next != nullptr) { // пока у последнего элемента следующий не равен указателю на пустоту
+        lastItem = lastItem->next; // последний элемент равен следующему за последним
     }
-    lastItem->next = newLast;
-    newLast->prev = lastItem;
+    lastItem->next = newLast; // следующий за последним элементом это новый последний
+    newLast->prev = lastItem; // элемент перед новым последним это последний
 
-    List *firstItem = item;
-    while (firstItem->prev != nullptr) {
-        firstItem = firstItem->prev;
+    List *firstItem = item; // созд указ на первый элемент списка
+    while (firstItem->prev != nullptr) // пока у первого элемента предшествующий  не рввен...
+        firstItem = firstItem->prev; // первый элемент равен предшествующему
     }
-    firstItem->prev = newFirst;
-    newFirst->next = firstItem;
+    firstItem->prev = newFirst; // элемент перед первым это новый первый
+    newFirst->next = firstItem; // элем следующ за новым первым это первый
 
-    first = newFirst;
+    first = newFirst; // в первый новый первый
 }
 
-void addToList() {
+void addToList() { //
     int toFirst, toLast;
     std::cout << "to first=";
     std::cin >> toFirst;
     std::cout << "to last=";
     std::cin >> toLast;
 
-    int elementIndex;
-    std::cout << "start index";
+    int elementIndex;// вводим индекс элемента который будет отправляться в функцию
+    std::cout << "start index=";
     std::cin >> elementIndex;
-    List *item = first;
+    List *item = first; // созаем указ на элемент с заданным индексом
     for (int i = 0; i < elementIndex; ++i) {
-        item = item->next;
+        item = item->next; // находим этот э6лемент с заданным индексом
     }
 
     addToList(item, toFirst, toLast);
